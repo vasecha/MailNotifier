79eed26d40062b2cf67cdc2fc33e7afbbaf94b9a
EinBaum <mail@einbaum.org>
2016-04-01 16:34:59 +0200

    Initial commit

---
+++ MailNotifier.lua
@@ -0,0 +1,38 @@
+﻿
+B_MN_VERSION = "1.0"
+
+
+-------------------------------------------------------------------------------
+
+
+--[[
+	Print text in the chat frame
+	Argument: msg - the text that you want to print
+]]
+function B_MN_Print(msg)
+	DEFAULT_CHAT_FRAME:AddMessage(msg, 0.37, 1, 0)
+end
+
+-------------------------------------------------------------------------------
+
+
+function B_MN_OnLoad()
+	this:RegisterEvent("ADDON_LOADED")
+	this:RegisterEvent("UPDATE_PENDING_MAIL")
+end
+
+function B_MN_OnEvent()
+	if (event == "ADDON_LOADED") then
+		if (string.lower(arg1) == "mailnotifier") then
+			B_MN_Print("MailNotifier " .. B_MN_VERSION .. " loaded.")
+		end
+	elseif (event == "UPDATE_PENDING_MAIL") then
+		if HasNewMail() then
+			B_MN_Frame:Show()
+		end
+	end
+end
+
+function B_MN_OnMouseDown()
+	B_MN_Frame:Hide()
+end
---
+++ README.md
@@ -0,0 +1,6 @@
+#MailNotifier#
+
+Fullscreen mail notifications. Useful for trading alts that just wait for auctions to sell.
+
+Your game window will have a green color when you receive mail.
+This notification is removed if you click any point of the screen with your mouse.
